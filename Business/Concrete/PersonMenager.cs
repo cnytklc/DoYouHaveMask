309d677569811be7847f38a19571006f94004dad
Cüneyt Kılıç <cnytklc10@gmail.com>
2024-03-13 19:03:24 +0300

    Does citizen have mask

---
+++ Business/Concrete/PersonMenager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Concrete;
+using MernisServiceReference1;
+
+namespace Business.Concrete
+{
+    public class PersonMenager : IApplicanService
+    {
+        public void ApplyForMask(Person person)
+        {
+
+        }
+
+        public List<Person> GetList()
+        {
+            return null;
+        }
+
+        public bool CheckPerson(Person person)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+            return client.TCKimlikNoDogrulaAsync(new TCKimlikNoDogrulaRequest
+                (new TCKimlikNoDogrulaRequestBody(person.NationalId,person.FirstName,person.LastName, person.BirthYear)))
+                .Result.Body.TCKimlikNoDogrulaResult;
+        }
+    }
+}
---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class PttManager : ISupplierService
+    {
+        private IApplicanService _applicanService;
+
+        public PttManager(IApplicanService applicanService) // Constructor new yapıldığında çalışır
+        {
+            _applicanService = applicanService;
+        }
+
+        public void GiveMask(Person person)
+        {
+            if (_applicanService.CheckPerson(person))
+            {
+                Console.WriteLine(person.FirstName + " için maske verildi");
+            }
+            else
+            {
+                Console.WriteLine(person.FirstName + " için maske verilemedi");
+
+            }
+        }
+    }
+}
